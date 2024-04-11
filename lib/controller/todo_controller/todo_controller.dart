cce7b4e87ca845f49e8e292952d67892fd5ed945
Princy <princyjames1810@gmail.com>
2024-04-11 18:55:56 +0530

    commit

---
+++ lib/controller/todo_controller/todo_controller.dart
@@ -0,0 +1,69 @@
+import 'package:hive/hive.dart';
+import 'package:my_app/model/todomodel/todo_model.dart';
+
+class TodoController {
+  List<TodoModel> todoList = [];
+  //refering the hive box
+  final mytodobox = Hive.box('todoBox');
+
+  //to add tasks in the todo list
+  void addTodo(TodoModel newtodo) {
+    todoList.add(newtodo);
+    updateDataBase();
+  }
+
+  //to remove tasks from the list
+  void deleteTodo(int index) {
+    todoList.removeAt(index);
+    updateDataBase();
+  }
+
+  //to load data from the database
+  void loadData() {
+    final retrievedData = mytodobox.get('TODOLIST') ?? [];
+    todoList = retrievedData.cast<TodoModel>(); // Type casting for safety
+  }
+  
+  //to edit the existing todotile
+  void editTodo(int index, TodoModel updatedTodo) {
+    todoList[index] = updatedTodo;
+    updateDataBase();
+  }
+
+//to update the database
+  void updateDataBase() {
+    mytodobox.put('TODOLIST', todoList);
+  }
+
+  //to count the completed tasks
+  int taskCountCompleted() {
+    int taskCompleted = 0;
+
+    for (int i = 0; i < todoList.length; i++) {
+      if (todoList[i].status == true) {
+        taskCompleted++;
+      }
+    }
+    return taskCompleted;
+  }
+
+  //to count the incompleted tasks
+  int taskCountIncompleted() {
+    int taskCompleted = 0;
+    int taskIncompleted;
+    for (int i = 0; i < todoList.length; i++) {
+      if (todoList[i].status == true) {
+        taskCompleted++;
+      }
+    }
+    taskIncompleted = todoList.length - taskCompleted;
+    return taskIncompleted;
+  }
+
+  //total task
+  int totalTask() {
+    int totalTask = 0;
+    totalTask = todoList.length;
+    return totalTask;
+  }
+}
---
+++ lib/view/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Image.asset('')),
+    );
+  }
+}
